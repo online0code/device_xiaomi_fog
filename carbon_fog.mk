dc53dbfa30668b1356ed4206fcc1f0a51d945919
online0code <151095682+online0code@users.noreply.github.com>
2025-02-20 06:40:39 +0700

    Update carbon_fog.mk change telephony part

--- carbon_fog.mk
+++ carbon_fog.mk
@@ -5,8 +5,11 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Inherit common.
 $(call inherit-product, vendor/carbon/config/common.mk)
