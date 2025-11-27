d9a0906c02806de09b28ae1e4bffc7e9abb69b18
online0code <151095682+online0code@users.noreply.github.com>
2025-11-27 22:07:52 +0000

    Revert "fog: ArrowOS Bring Up!"
    This reverts commit 90d60a641902bcb5b650b6f3ea8b9cba09ebfea8.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_fog.mk
+    $(LOCAL_DIR)/lineage_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_fog-user \
-    arrow_fog-userdebug \
-    arrow_fog-eng
+    lineage_fog-user \
+    lineage_fog-userdebug \
+    lineage_fog-eng
--- lineage_fog.mk
+++ lineage_fog.mk
@@ -8,12 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Arrow stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
-
-# ArrowOS Additional Flags
-DEVICE_MAINTAINER := Iverz
-DEVICE_MAINTAINER_LINK := https://t.me/JustVerz
+# Inherit some common stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
@@ -21,7 +17,7 @@ TARGET_SUPPORTS_QUICK_TAP := false
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-PRODUCT_NAME := arrow_fog
+PRODUCT_NAME := lineage_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
