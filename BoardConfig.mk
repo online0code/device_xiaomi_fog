7e5270a3ba5072e9127570358aa9fda5cd182d34
online0code <151095682+online0code@users.noreply.github.com>
2025-11-28 05:43:02 +0000

    fix wrong space

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,10 +120,10 @@ BOARD_KERNEL_CMDLINE += \
     lpm_levels.sleep_disabled=1 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \ 
+    video=vfb:640x400,bpp=32,memsize=3072000 \
     swiotlb=2048 \
     selinux=1 \
-    audit=0 
+    audit=0
 
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 NEED_KERNEL_MODULE_SYSTEM := true
