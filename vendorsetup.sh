b3e26bb98d43bf3f9e10ce1af7def97d4a86ee6d
online0code <151095682+online0code@users.noreply.github.com>
2025-11-29 23:46:17 +0000

    Add command for lineage-20.0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,9 @@ rm hardware/google/pixel/kernel_headers/Android.bp
 # Kernel fixups
 rm kernel/xiaomi/fog/Android.bp
 rm kernel/xiaomi/fog/techpack/audio/Android.bp
+
+curDir=`pwd`
+if [ "$non_hybris" = "yes" ]; then
+   wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.1/BoardConfig.mk && mv BoardConfig.mk device/xiaomi/fog/
+   cd kernel/xiaomi/fog && rm -rf KernelSU-Next && curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash - && cd $curDir
+fi
